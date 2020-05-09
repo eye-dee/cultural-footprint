fe38cad82d59d8164e089fe406057805ecca8140
Roman Golovin <roman_gr@mail.com>
2020-05-09 20:51:34 +0300

    Remove * in imports once again

--- src/main/java/de/egor/culturalfootprint/admin/controller/ClusterController.kt
+++ src/main/java/de/egor/culturalfootprint/admin/controller/ClusterController.kt
@@ -4,7 +4,11 @@ import de.egor.culturalfootprint.admin.dto.ClusterResult
 import de.egor.culturalfootprint.model.Cluster
 import de.egor.culturalfootprint.service.ClusterService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
